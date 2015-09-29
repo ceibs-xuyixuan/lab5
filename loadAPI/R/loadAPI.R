42b5dff377236df20953066dd9d2d8ee1e12efcc
xuyixuan <yixxu916@student.liu.se>
2015-09-29 08:48:28 +0200

    updata some

--- loadAPI/R/loadAPI.R
+++ loadAPI/R/loadAPI.R
@@ -5,13 +5,19 @@
 #' @param address the address is real address that you wish to search.
 
 geocode_GET <- function(address){
-  address <- gsub("\\s", "+", address)
-  link <- paste("https://maps.googleapis.com/maps/api/geocode/json?","address=", address, "&key=AIzaSyCoWpv7VCmdP_EZLEidQzUpxyOC-HC5Ui8", sep="")
+  address1 <- gsub("\\s", "+", address)
+  link <- paste("https://maps.googleapis.com/maps/api/geocode/json?","address=", address1, "&key=AIzaSyCoWpv7VCmdP_EZLEidQzUpxyOC-HC5Ui8", sep="")
   request <- getURL(link)
   # request
   geo <- fromJSON(request)
-  geo
+  # geo
+  if(geo$status == "OK"){
+    lat <- geo$results[[3]]$location$lat
+    lng <- geo$results[[3]]$location$lng
+    return (c(lat, lng, address))
+  }
 }
+
 # ## an example
 # jsonData <- fromJSON("https://maps.googleapis.com/maps/api/geocode/json?address=santa+cruz&components=country:ES&key=AIzaSyCoWpv7VCmdP_EZLEidQzUpxyOC-HC5Ui8")
 # names(jsonData)
