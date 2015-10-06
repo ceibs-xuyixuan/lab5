eaac9f32495132a02d4bf62df42eb63c997efff7
xuyixuan <xuyixuan@users.noreply.github.com>
2015-10-06 11:27:47 +0200

    Update testthat.R

--- loadAPI/tests/testthat/testthat.R
+++ loadAPI/tests/testthat/testthat.R
@@ -1,6 +1,6 @@
-library("jsonlite", lib.loc="~/R/win-library/3.2")
-library("bitops", lib.loc="~/R/win-library/3.2")
-library("RCurl", lib.loc="~/R/win-library/3.2")
+library("jsonlite")
+library("bitops")
+library("RCurl")
 context("looking for error")
 test_that("error with address",{
   expect_that (as.numeric(geocode_GET("linkoping")[1, 1]), equals
